328b82f0f7b530b7110dddcf420891def7327022
DanielRGilles <danryangilles@gmail.com>
2021-07-15 10:58:48 -0700

    built the animal farm

---
+++ app.js
@@ -0,0 +1,27 @@
+// grab DOM elements
+const h1El = document.getElementById('animal-name');
+const horseEl = document.getElementById('horse-image');
+const dogEl = document.getElementById('dog-image');
+const catEl = document.getElementById('cat-image');
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
+
+
+// set event listeners 
+horseEl.addEventListener('click', () => {
+    horseSound.play();
+    h1El.textContent = 'Horse'
+    
+});
+
+dogEl.addEventListener('click', () => {
+    dogSound.play();
+    h1El.textContent = 'Dog'
+});
+
+catEl.addEventListener('click', () => {
+    catSound.play();
+    h1El.textContent = 'Cat'
+    
+});
